60d740b7693c356e2e559596f89880b3a9c44092
fillipeab <filipe.spy.br@gmail.com>
2025-12-21 14:00:29 -0300

    now yes

--- LineWatcher.py
+++ LineWatcher.py
@@ -1,26 +1,47 @@
 ###LINE WATCHER - THE END OF OUR JORNEY
 from dataclasses import dataclass, field
 from typing import Any
+from scipy.stats import trim_mean
 from scipy.spatial import cKDTree
 import numpy as np
 
 @dataclass
 class LineWatcher():
     ### receives the list of people
-
-
-def find_nearest_neighbors(list_of_temporary_people, n=2):
-    """Returns the n nearest BBoxes for each BBox."""
-    # Calculate BBox centers
-    centers = np.array([
-        [(x1 + x2) / 2, (y1 + y2) / 2]
-        for person in list_of_temporary_people
-        for x1, y1, x2, y2 in [person.bbox]
-    ])
+    NEIGHBOUR_MAX_RADIUS_DISTANCE : int   = 1   ###How distance, in smaller_axis value, can a neighbour be - This will be passed to find_nearest_neighbors
+    PERCENT_CUT_TRIM_MEAN         : float = 0.1
+    ###static method
+    @staticmethod
+    def find_nearest_neighbors_by_radius(list_of_temporary_people, radius=50):
+        """People within radius of each person based on BBox centers."""
+        # Calculate centers from BBoxes
+        centers = np.array([
+            [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
+            for person in temporary_people_list
+            for bbox in [person.bbox]
+        ])
+        
+        tree = cKDTree(centers)
+        neighbors = {}
+        
+        for i, center in enumerate(centers):
+            indices = tree.query_ball_point(center, radius)
+            indices.remove(i)  # Remove self
+            neighbors[i] = indices
+        return neighbors
     
-    # Build KDTree for fast nearest neighbor search
-    tree = cKDTree(centers)
-    _, indices = tree.query(centers, k=n+1)  # +1 to include self
+    def find_smaller_axis_of_people(self, list_of_temporary_people, percent_cut = self.PERCENT_CUT_TRIM_MEAN):
+        smaller_dimension_list = []
+        for person in list_of_temporary_people:
+            bbox = person.bb ###Its in x1,y1,x2,y2 format
+            smaller_dimension = x_variation = abs(bbox[0] - bbox[2])
+            y_variation = abs(bbox[1] - bbox[3])
+            if x_variation>y_variation:
+                smaller_dimension = y_variation
+            ###add to list
+            smaller_dimension_list.append(smaller_dimension)
+        trim_mean(smaller_dimension_list, percent_cut) ###Finds the mean, cutting extremes
     
-    # Remove self from each list
-    return {i: idxs[1:].tolist() for i, idxs in enumerate(indices)}
+    
+    def __call__(list_of_temporary_people): ###LineWatcher is called
+        
